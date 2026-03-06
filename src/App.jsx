531f8ed3b369511af68fc6e95ac748fb80f15e1f
nabil-hajali <nabil2491@gmail.com>
2026-03-06 15:42:44 +0100

    add the AppHeader/Main/Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import AppMain from "./components/AppMain"
+import AppFooter from "./components/AppFooter"
+import AppHeader from "./components/AppHeader"
 
 function App() {
  
 
   return (
     <>
-     
+   <AppHeader />
+   <AppMain />
+   <AppFooter />
     </>
   )
 }
